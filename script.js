8acd779679563a1f22b48287e9a1a2d477bade96
Ganesh Prasannan <93814719+GaneshPrasannan@users.noreply.github.com>
2021-12-01 17:58:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var webhookURL = "https://discord.com/api/webhooks/913772318713585714/lJsp4WyeMbZLlKFCUXdg5b8EQEmuiKGi2fGcMSCNbtdKWgKSYD1pjscYVPFB5Ki7nyH0"
+var webhookURL = "{webhookURL}"
 var webhookUsername = "Contact Form Bot"
 var webhookAvatar = "https://i.pinimg.com/originals/88/42/da/8842dabf70fd5dea2bc6c9673d74bc51.png"
 
@@ -18,4 +18,3 @@ $(function() {
 function submitted(){
     document.getElementById('status').style.display = "block"
 }
-
